5069c7a2355cb37cadf7ffbf4cb88e982ba8fefd
tigerabrodi <tigerabrodi@gmail.com>
2022-08-30 06:42:49 +0200

    refactor: grey color and navigation files

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -4,7 +4,7 @@ import './Navigation.css'
 export function Navigation() {
   return (
     <nav class="navigation__wrapper">
-      <a href="/" class="navigation__link">
+      <a href="/" class="navigation__link" aria-label="to home">
         <NarutoMainArt class="navigation__art" />
       </a>
     </nav>
