172d644c232967a841a5ce28da79f999aa114daa
tigerabrodi <tigerabrodi@gmail.com>
2022-09-03 22:28:57 +0200

    deploy to edge?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config'
-import netlify from '@astrojs/netlify/functions'
+import netlify from '@astrojs/netlify/edge-functions'
 import solid from '@astrojs/solid-js'
 
 // https://astro.build/config
