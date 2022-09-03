ef1775188a9934bcd93bdf764deb3ff515d5c358
tigerabrodi <tigerabrodi@gmail.com>
2022-09-03 18:02:04 +0200

    feat: add most of  movie player

---
+++ src/components/Movie/Movie.tsx
@@ -0,0 +1,143 @@
+import { createSignal, Show } from 'solid-js'
+import { BackButton } from '../../icons/BackButton'
+import { Backward } from '../../icons/Backward'
+import { Forward } from '../../icons/Forward'
+import { Play } from '../../icons/Play'
+import { Stop } from '../../icons/Stop'
+import { VolumeEmpty } from '../../icons/VolumeEmpty'
+import { VolumeFull } from '../../icons/VolumeFull'
+import type { MovieWithMovieImage } from '../../lib/schemas'
+import './Movie.css'
+
+type MovieProps = {
+  movie: MovieWithMovieImage
+}
+
+export function Movie(props: MovieProps) {
+  const [isMoviePlaying, setIsMoviePlaying] = createSignal(true)
+  const [isMovieMuted, setIsMovieMuted] = createSignal(false)
+  const [currentProgress, setCurrentProgress] = createSignal(0)
+  let videoElement: HTMLVideoElement | undefined = undefined
+
+  function togglePauseState() {
+    if (videoElement) {
+      if (isMoviePlaying()) {
+        videoElement.pause()
+        setIsMoviePlaying(false)
+      } else {
+        videoElement.play()
+        setIsMoviePlaying(true)
+      }
+    }
+  }
+
+  function toggleVolumeState() {
+    if (videoElement) {
+      if (isMovieMuted()) {
+        videoElement.volume = 1
+        setIsMovieMuted(false)
+      } else {
+        videoElement.volume = 0
+        setIsMovieMuted(true)
+      }
+    }
+  }
+
+  function handleSkipBySeconds(secondsToBeSkipped: number) {
+    if (videoElement) {
+      videoElement.currentTime += secondsToBeSkipped
+    }
+  }
+
+  function handleProgressSliderChange(
+    event: Event & { currentTarget: HTMLInputElement; target: Element }
+  ) {
+    const roundedProgress = Math.round(
+      Number((event.target as HTMLInputElement).value)
+    )
+
+    const percentage = roundedProgress / 100
+    if (videoElement) {
+      const newTime = percentage * videoElement.duration
+
+      videoElement.currentTime = newTime
+      setCurrentProgress(roundedProgress)
+    }
+  }
+
+  return (
+    <main class="movie__container">
+      <a class="movie__back-link" href="/">
+        <BackButton />
+      </a>
+      <video
+        ref={videoElement}
+        src={props.movie.movieUrl}
+        autoplay
+        class="movie__video"
+        preload="metadata"
+        muted
+        poster={props.movie.imageUrl}
+      />
+      <div class="movie__player">
+        <div class="movie__slider-wrapper">
+          <input
+            type="range"
+            name="progress"
+            min="0"
+            max="100"
+            step="0.1"
+            class="movie__slider"
+            onInput={handleProgressSliderChange}
+            value={currentProgress()}
+          />
+          <div
+            class="movie__slider-progress"
+            style={{
+              width: `${currentProgress()}%`,
+            }}
+          />
+        </div>
+
+        <h1 class="movie__player-title">{props.movie.title}</h1>
+        <div class="movie__player-buttons">
+          <button
+            class="movie__player-button"
+            onClick={togglePauseState}
+            aria-label={isMoviePlaying() ? 'Stop movie' : 'Play movie'}
+          >
+            <Show when={isMoviePlaying()} fallback={<Play />}>
+              <Stop />
+            </Show>
+          </button>
+
+          <button
+            class="movie__player-button"
+            aria-label="Go back 10 seconds"
+            onClick={() => handleSkipBySeconds(-10)}
+          >
+            <Backward />
+          </button>
+
+          <button
+            class="movie__player-button"
+            aria-label="Go forward 10 seconds"
+            onClick={() => handleSkipBySeconds(10)}
+          >
+            <Forward />
+          </button>
+
+          <button
+            class="movie__player-button"
+            onClick={toggleVolumeState}
+            aria-label={isMovieMuted() ? 'Unmute movie' : 'Mute movie'}
+          >
+            <Show when={isMovieMuted()} fallback={<VolumeFull />}>
+              <VolumeEmpty />
+            </Show>
+          </button>
+        </div>
+      </div>
+    </main>
+  )
+}
---
+++ src/icons/Stop.tsx
@@ -0,0 +1,17 @@
+export function Stop() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M4.5 3C4.22386 3 4 3.22386 4 3.5V20.5C4 20.7761 4.22386 21 4.5 21H9.5C9.77614 21 10 20.7761 10 20.5V3.5C10 3.22386 9.77614 3 9.5 3H4.5ZM14.5 3C14.2239 3 14 3.22386 14 3.5V20.5C14 20.7761 14.2239 21 14.5 21H19.5C19.7761 21 20 20.7761 20 20.5V3.5C20 3.22386 19.7761 3 19.5 3H14.5Z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  )
+}
